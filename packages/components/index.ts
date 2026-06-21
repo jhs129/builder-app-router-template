5d212f319d1a456077a24d39c6c07375e763b949
jhs-radical <jschneider@radicaldesign.co>
2026-06-21 14:53:09 -0400

    plugin styling

--- packages/components/index.ts
+++ packages/components/index.ts
@@ -12,6 +12,7 @@ export * from './components/ui/Accordion';
 export * from './components/ui/Alert';
 export * from './components/ui/Button';
 export * from './components/ui/DesignKitOverview';
+export * from './components/ui/DynamicLink';
 export * from './components/ui/Headline';
 export * from './components/ui/NotFoundContent';
 
